ef3c73e7a35db8a338f60b83594ef397afe1f4de
Anzhelika <anzhelika.flyorko@gmail.com>
2022-10-27 04:58:33 +0300

    feat:добавлены страницы Trade-in, Избранное, Карточка товара,Автокредит

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as myFunctions from "./modules/main.js";
 import * as bannerSlider from "./modules/banner-slider.js";
+import * as autoSlider from "./modules/auto-page-slider.js";
 
 bannerSlider.addBannerSlider()
 flsFunctions.isWebp();
 myFunctions.addMainScript();
+autoSlider.addAutoSlider();
 
-
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
---
+++ src/js/modules/auto-page-slider.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+import "slick-carousel";
+
+export function addAutoSlider() {
+
+
+// $(".slider").slick({
+//     dots:true,
+// });
+$(".slider-otzyvy").slick({
+    dots:true,
+    arrows:true,
+});
+}
