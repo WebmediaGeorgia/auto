d18d101f2ce1a881330230c0f25d0c8240c40bec
Anzhelika <anzhelika.flyorko@gmail.com>
2022-10-31 04:35:05 +0300

    feat:добавлен знак webmedia в футер, добавлен адаптив 768px(главная страница)

--- src/js/modules/close-top-banner.js
+++ src/js/modules/close-top-banner.js
@@ -2,10 +2,18 @@ export function closeTopBanner() {
   let topBanner = document.querySelector(".header .header__top");
   let closeTop = document.querySelector(".item.close");
   let main = document.querySelector('main');
+  const mediaQuery = window.matchMedia('(max-width:1200px)')
+
 
   closeTop.addEventListener("click", () => {
     topBanner.classList.add("close-banner");
-    main.style.marginTop = '5.365vw'
+    if(mediaQuery.matches){
+      main.style.marginTop = '13.151vw'
+    }else{
+     main.style.marginTop = '5.365vw' 
+    }
+    
+
 
 
   });
