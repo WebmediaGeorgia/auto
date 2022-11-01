19ecbeab42a6e65f83af7c63b6f9cdb37008c1cd
Anzhelika <anzhelika.flyorko@gmail.com>
2022-11-02 01:55:54 +0300

    feat:добавлен адаптив форм и работа всплывающих окон на 768px

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -1,5 +1,29 @@
-
 export function addMainScript() {
-console.log('ура')
+  const form_all = document.querySelectorAll(" form");
 
+  form_all.forEach((item) => {
+    item.addEventListener("submit", sendForm);
+  });
+
+  function sendForm(e) {
+    e.preventDefault();
+    e.target.reset();
+    lookPopapSuccess();
+  }
+  // popap
+  const popap = document.querySelector(".popup-for-successful");
+  let closePopap = document.querySelector(".popup-for-successful .popup_close");
+  let backPopap = document.querySelector(".popup-for-successful .back-to-main");
+
+  function lookPopapSuccess() {
+    popap.classList.add("open");
+    closePopap.addEventListener("click", function (e) {
+      popap.classList.remove("open");
+      e.preventDefault();
+    });
+    backPopap.addEventListener("click", function (e) {
+      popap.classList.remove("open");
+      e.preventDefault();
+    });
+  }
 }
